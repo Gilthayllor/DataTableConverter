ed1f090b821e4706eaf534350d0b583e0aab45cc
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-04-14 19:30:30 -0300

    Attribute name changed

---
+++ DataTableConverter/Attributes/DataTableColumnAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataTableConverter.Attributes
+{
+    /// <summary>
+    /// Attribute used to specify the name of the column in the <see cref="System.Data.DataTable"/> that corresponds to the decorated property.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class DataTableColumnAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the name of the column in the <see cref="System.Data.DataTable"/> that corresponds to the decorated property.
+        /// </summary>
+        public string ColumnName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataTableColumnAttribute"/> class with the specified column name.
+        /// </summary>
+        /// <param name="columnName">The name of the column in the DataTable that corresponds to the decorated property.</param>
+        public DataTableColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+
+    }
+}
