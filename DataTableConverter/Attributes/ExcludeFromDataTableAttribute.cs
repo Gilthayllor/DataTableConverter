480866a25606b038d0932db0f885c103ec6821e5
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-04-14 19:31:01 -0300

    Attribute to exclude column created

---
+++ DataTableConverter/Attributes/ExcludeFromDataTableAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataTableConverter.Attributes
+{
+    /// <summary>
+    /// Indicates that a property should be excluded when converting an object to a DataTable.
+    /// </summary>
+    public class ExcludeFromDataTableAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExcludeFromDataTableAttribute"/> class.
+        /// </summary>
+        public ExcludeFromDataTableAttribute() { }
+    }
+}
