e5c1b459da6c232095ab723255de3c10747bfc04
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-04-14 14:17:58 -0300

    Add project files.

---
+++ DataTableConverter/Attributes/DataTableAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataTableConverter.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class DataTableColumnAttribute : Attribute
+    {
+        public string ColumnName { get; set; }
+        public DataTableColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
