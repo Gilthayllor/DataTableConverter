f6cac4356a44b30b5a7ce43fc04c7073d555aec3
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-04-14 16:02:46 -0300

    DataTable and PropertyInfo extensions

---
+++ DataTableConverter/Extensions/DataTableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DataTableConverter.Extensions
+{
+    internal static class DataTableExtensions
+    {
+        internal static DataColumn GetColumn(this List<DataColumn> cols, string colName)
+        {
+            return cols.FirstOrDefault(c => c.ColumnName == colName);
+        }
+
+        internal static List<DataColumn> GetColumns(this DataTable dataTable)
+        {
+            return dataTable.Columns.OfType<DataColumn>().ToList();
+        }
+
+        internal static List<DataColumn> GetColumns(this DataRow row)
+        {
+            return row.Table.Columns.OfType<DataColumn>().ToList();
+        }
+    }
+}
---
+++ DataTableConverter/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DataTableConverter.Attributes;
+using System.Reflection;
+
+namespace DataTableConverter.Extensions
+{
+    internal static class PropertyInfoExtensions
+    {
+        internal static string GetColumnName(this PropertyInfo property)
+        {
+            string columnName = property.Name;
+
+            DataTableColumnAttribute columnAttribute = property.GetCustomAttribute<DataTableColumnAttribute>();
+
+            if (columnAttribute != null && !string.IsNullOrWhiteSpace(columnAttribute.ColumnName))
+                columnName = columnAttribute.ColumnName;
+
+            return columnName;
+        }
+    }
+}
