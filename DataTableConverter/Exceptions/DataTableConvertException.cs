15de2c9f86f964ca6fe1d232015ddf99b8ba8166
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-04-17 09:09:11 -0300

    Custom exceptions added

---
+++ DataTableConverter/Exceptions/DataTableConvertException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTableConverter.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when an error occurs during the conversion of a <see cref="System.Data.DataTable"/> to an object or a list of objects.
+    /// </summary>
+    public class DataTableConvertException : Exception
+    {
+        public DataTableConvertException(string message) : base(message) { }
+
+        public DataTableConvertException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+        public DataTableConvertException(Exception innerException) : this("An exception occurred while attempting to convert the object. See the inner exception for more details.", innerException)
+        {
+
+        }
+    }
+}
---
+++ DataTableConverter/Exceptions/NoMatchingColumnException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataTableConverter.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when no matching column is found in a <see cref="System.Data.DataTable"/> while converting to an object or a list of objects.
+    /// </summary>
+    public class NoMatchingColumnException : Exception
+    {
+        public NoMatchingColumnException(string message) : base(message) { }
+    }
+}
